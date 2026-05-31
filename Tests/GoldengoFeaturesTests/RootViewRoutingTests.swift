e1afaf5aef7760095a739db7deb7d12daead9bda
Jurgen <lekacoding@gmail.com>
2026-05-31 14:41:15 +0200

    test: cover goldengo://settings deep link; ignore .build-old (GOL-35 review)

--- Tests/GoldengoFeaturesTests/RootViewRoutingTests.swift
+++ Tests/GoldengoFeaturesTests/RootViewRoutingTests.swift
@@ -5,6 +5,7 @@ final class RootViewRoutingTests: XCTestCase {
     func test_deepLink_routesToExpectedTabs() {
         XCTAssertEqual(RootView.tab(forDeepLink: URL(string: "goldengo://quickadd")!), 0)
         XCTAssertEqual(RootView.tab(forDeepLink: URL(string: "goldengo://recent")!), 1)
+        XCTAssertEqual(RootView.tab(forDeepLink: URL(string: "goldengo://settings")!), 2)
         XCTAssertNil(RootView.tab(forDeepLink: URL(string: "goldengo://unknown")!))
         XCTAssertNil(RootView.tab(forDeepLink: URL(string: "https://quickadd")!))
     }
